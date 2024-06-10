1b9502e9a9bb7c4f41500789c71f0491bcb98380
Houdini Papi <ochiengcliff.co@gmail.com>
2024-06-11 01:36:32 +0300

    chore: Update CORS allowed origins in settings.py
    This commit updates the `CORS_ALLOWED_ORIGINS` list in the `settings.py` file of the `podcast.podcast` module. It adds "http://127.0.0.1.3000" and "http://localhost:3000" as allowed origins, allowing requests from these URLs. This change improves the functionality and accessibility of the application.
    
    Note: The suggested commit message follows the established convention of using a prefix (chore) to indicate a chore or maintenance task.

--- podcast/podcast/settings.py
+++ podcast/podcast/settings.py
@@ -183,7 +183,8 @@ SIMPLE_JWT = {
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
-    "http://127.0.0.1.8000",
+    "http://127.0.0.1.3000",
+    "http://localhost:3000",
 ]
 
 PASSWORD_RESET_TIMEOUT = 900
