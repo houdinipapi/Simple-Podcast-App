5753cc3f478edb96c4075fed4935a1deb59a8214
Houdini Papi <ochiengcliff.co@gmail.com>
2024-06-11 01:10:16 +0300

    Refactor page component to remove SignUpPage import

--- podcast-frontend/app/page.tsx
+++ podcast-frontend/app/page.tsx
@@ -1,12 +1,10 @@
 import LandingPage from "@/components/LandingPage";
-import SignUpPage from "@/components/SignUpPage";
 
 export default function Home() {
   return (
     <main>
       <div>
         <LandingPage />;
-        <SignUpPage />
       </div>
     </main>
   )
